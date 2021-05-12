64cf9d8dcb1fd0a4712117a60f0c20fe9db84bf7
jim mccann <mccann.jim@gmail.com>
2021-05-12 16:32:08 +0000

    added the fixed label

--- main.tf
+++ main.tf
@@ -38,8 +38,7 @@ resource "tls_private_key" "ssh-key" {
 resource "google_compute_instance" "hashicat" {
   name         = "${var.prefix}-hashicat"
   zone         = "${var.region}-b"
-  department   = "devops"
-  billable     = true
+
   machine_type = var.machine_type
 
   boot_disk {
